1a0fbe995fa8f96d8a01652f769cd1d63b0187fb
5591rolimishra <112165524+5591rolimishra@users.noreply.github.com>
2022-08-26 17:30:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit_app.header('Breakfast Menu')
+streamlit_app.text('Omega 3 & Blueberry Oatmeal')
+streamlit_app.text('Kale, Spinach & Rocket Smoothie')
+streamlit_app.text('Hard-Boiled Free-Range Egg')
