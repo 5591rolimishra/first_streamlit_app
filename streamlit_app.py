65e7bb13295b9f5b219c5fd850305014169907c0
5591rolimishra <112165524+5591rolimishra@users.noreply.github.com>
2022-09-07 16:21:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,3 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
-add_my_fruit_response = requests.get("https://fruityvice.com/api/fruit/"+add_my_fruit)
-add_my_fruit_normalized = pandas.json_normalize(add_my_fruit_response.json())
-streamlit.dataframe(add_my_fruit_normalized)
